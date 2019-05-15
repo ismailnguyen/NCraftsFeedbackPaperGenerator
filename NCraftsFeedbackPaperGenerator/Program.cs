87dc051170ce5fa78cc136c8ca2740ffd5cf3741
Ismaïl <nguyen.ismail@gmail.com>
2019-05-15 20:08:42 +0200

    Add project files.

---
+++ NCraftsFeedbackPaperGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NCraftsFeedbackPaperGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
